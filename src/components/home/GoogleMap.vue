42debb0c58d45226d189d9cd2bd77228390762d5
Christos Giannouris <chris1giann1@gmail.com>
2019-10-09 18:05:51 +0200

    rendering Google Maps

--- src/components/home/GoogleMap.vue
+++ src/components/home/GoogleMap.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="map">
-        <h2>Map</h2>
+        <div class="google-map" id="map"></div>
     </div>
 </template>
 
@@ -8,10 +8,37 @@
 export default {
     name: "GoogleMap",
     data() {
-        return {};
+        return {
+            lat: 50,
+            lng: 17
+        };
+    },
+    methods: {
+        renderMap() {
+            const map = new google.maps.Map(document.getElementById("map"), {
+                center: { lat: this.lat, lng: this.lng },
+                zoom: 5,
+                maxZoom: 15,
+                minZoom: 3,
+                streetViewControl: false
+            });
+        }
+    },
+    mounted() {
+        this.renderMap();
     }
 };
 </script>
 
 <style scoped>
+.google-map {
+    width: 100%;
+    height: 100%;
+    margin: 0 auto;
+    background: #fff;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: -1;
+}
 </style>
