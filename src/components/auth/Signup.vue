1d0841810e3cb1900aee76bd1d2e948ef4cfb41f
Christos Giannouris <chris1giann1@gmail.com>
2019-10-12 09:44:19 +0200

    signup Form

--- src/components/auth/Signup.vue
+++ src/components/auth/Signup.vue
@@ -1,33 +1,89 @@
 <template>
     <div class="signup container">
-        <form class="card-pane1">
+        <form @submit.prevent="signup" class="card-pane1">
             <h2 class="center deep-purple-text">Signup</h2>
             <div class="field">
                 <label for="email">Email</label>
-                <input type="email" name="email" />
+                <input type="email" name="email" v-model="email" />
             </div>
             <div class="field">
                 <label for="password">Password</label>
-                <input type="password" name="password" />
+                <input type="password" name="password" v-model="password" />
             </div>
             <div class="field">
                 <label for="alias">Alias</label>
-                <input type="alias" name="alias" />
+                <input type="text" name="alias" v-model="alias" />
             </div>
+            <p class="red-text center" v-if="feedback">{{feedback}}</p>
             <div class="field center">
-                <button class="btn"></button>
+                <button class="btn deep-purple">Signup</button>
             </div>
         </form>
     </div>
 </template>
 
 <script>
+import slugify from "slugify";
+import db from "@/firebase/init";
+
+import firebase from "firebase";
+
 export default {
     name: "Signup",
     data() {
-        return {};
+        return {
+            email: null,
+            password: null,
+            alias: null,
+            feedback: null,
+            slug: null
+        };
+    },
+    methods: {
+        signup() {
+            if (this.alias && this.email && this.password) {
+                this.slug = slugify(this.alias, {
+                    replacement: "-",
+                    remove: /[$*_+~.()'"!\-:@]/g,
+                    lower: true
+                });
+                let ref = db.collection("users").doc(this.slug);
+                ref.get().then(doc => {
+                    if (doc.exists) {
+                        this.feedback = "this alias already exists";
+                    } else {
+                        firebase
+                            .auth()
+                            .createUserWithEmailAndPassword(
+                                this.email,
+                                this.password
+                            )
+                            .then(() => (this.feedback = ""))
+                            .catch(err => {
+                                console.log(err);
+                                this.feedback = err.message;
+                            });
+                    }
+                });
+                console.log(this.slug);
+            } else {
+                this.feedback = "You must enter all fields";
+            }
+        }
     }
 };
 </script>
 <style scoped>
+.signup {
+    max-width: 400px;
+    margin-top: 60px;
+}
+
+.signup h2 {
+    font-size: 2.4em;
+}
+
+.signup .field {
+    margin-bottom: 16px;
+}
 </style>
