8549f88eed604fe21555655bc9e2424102358636
Christos Giannouris <chris1giann1@gmail.com>
2019-10-12 08:26:08 +0200

    rendering Google Maps

---
+++ src/components/auth/Signup.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="signup container">
+        <form class="card-pane1">
+            <h2 class="center deep-purple-text">Signup</h2>
+            <div class="field">
+                <label for="email">Email</label>
+                <input type="email" name="email" />
+            </div>
+            <div class="field">
+                <label for="password">Password</label>
+                <input type="password" name="password" />
+            </div>
+            <div class="field">
+                <label for="alias">Alias</label>
+                <input type="alias" name="alias" />
+            </div>
+            <div class="field center">
+                <button class="btn"></button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Signup",
+    data() {
+        return {};
+    }
+};
+</script>
+<style scoped>
+</style>
