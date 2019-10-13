5bd0dcdd83f3cfc9ecb46ed33afbda9bdd770095
Christos Giannouris <chris1giann1@gmail.com>
2019-10-13 15:55:02 +0200

    add markers

--- src/components/home/GoogleMap.vue
+++ src/components/home/GoogleMap.vue
@@ -24,6 +24,27 @@ export default {
                 minZoom: 3,
                 streetViewControl: false
             });
+
+            db.collection("users")
+                .get()
+                .then(users => {
+                    users.docs.forEach(doc => {
+                        let data = doc.data();
+                        if (data.geolocation) {
+                            let marker = new google.maps.Marker({
+                                position: {
+                                    lat: data.geolocation.lat,
+                                    lng: data.geolocation.lng
+                                },
+                                map: map
+                            });
+                            // add click event to the marker
+                            marker.addListener("click", () => {
+                                console.log(doc.id);
+                            });
+                        }
+                    });
+                });
         }
     },
     mounted() {
